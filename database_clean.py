d1220595a2ab5ba0264096152a3cadf4fd8813b3
dh493 <dh493@cam.ac.uk>
2016-11-25 16:22:04 +0000

    Database Changes

--- database_clean.py
+++ database_clean.py
@@ -2,6 +2,7 @@
 Performs SQLite3 operations on a database
 """
 
+import argparse
 import sqlite3
 
 from rinchi_tools import v02_convert
@@ -23,5 +24,18 @@ def gen_rauxinfo(db_filename, table_name):
         return rauxinfo
 
     db.create_function("convert", 1, converter)
-    cursor.execute("UPDATE ? SET rauxinfo = convert(rinchi) WHERE rauxinfo IS NULL or rauxinfo = '';", table_name)
+    cursor.execute("UPDATE ? SET rauxinfo = convert(rinchi) WHERE rauxinfo IS NULL or rauxinfo = '';", [table_name])
     return
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Database Tools - Duncan Hampshire 2016")
+    parser.add_argument("database", help="Database File")
+    parser.add_argument("tablename", help="Table Name")
+
+    args = parser.parse_args()
+    print(args.database, args.tablename)
+
+    if True:
+        print("Generating RAuxInfos")
+        gen_rauxinfo(args.database,args.tablename)
+        print("Done")
