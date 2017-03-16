071133dbf9fc485bd151be5668e32e51cd63be06
dh493 <dh493@cam.ac.uk>
2017-03-16 18:06:09 +0000

    fixed changes

--- rinchi_tools/tools.py
+++ rinchi_tools/tools.py
@@ -705,7 +705,8 @@ def process_stats(rinchis, mostcommon=None):
 
     data['pops'] = data['reactants'] + data['products'] + data['agents']
     if mostcommon is not None:
-        print(data.items())
+        for k,v in data.items():
+            print(k, Counter(dict(v.most_common(mostcommon))))
         data = {k: Counter(dict(v.most_common(mostcommon))) for k, v in data.items()}
 
     return data
